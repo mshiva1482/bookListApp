2785febc360dff1dc71b10cd75b3a57274293051
shiva <mshiva1482@gmail.com>
2021-05-25 08:16:50 +0530

    Completed!

---
+++ app.js
@@ -0,0 +1,140 @@
+// Book class
+class Book{
+    constructor(title, author, isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+//UI class
+class UI{
+    static displayBooks(){
+        const books = Store.getBooks();
+        books.forEach((book) => UI.addBookToList(book));
+    }
+    static addBookToList(book){
+        const list = document.querySelector('#book-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="rembtn">X</a></td>
+        `;
+        list.appendChild(row);
+    }
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `${className}`;
+        div.appendChild(document.createTextNode(message));
+
+
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form')
+        container.insertBefore(div, form);
+
+        setTimeout(()=>div.remove(), 2000)
+    }
+    static clearFields(){
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
+    static deleteBook(el){
+        if(el.classList.contains('rembtn')){
+            el.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// //Storage class
+// class store{
+//     static getBooks(){
+//         let books;
+//         if(localStorage.getItem() === null){
+//             books = [];
+//         }
+//         else{
+//             books = JSON.parse(localStorage.getItem('books'))
+//         }
+//         return books;
+//     }
+
+//     static addBook(book){
+//         const books = store.getBooks();
+//         books.push(book);
+//         localStorage.setItem('books', JSON.stringify(books));
+//     }
+//     static removeBook(isbn){
+//         const books = store.getBooks();
+//         books.forEach((book, index) =>{
+//             if(book.isbn == isbn){
+//                 books.splice(index, 1);
+//             }
+//         });
+//         localStorage.setItem('books', JSON.stringify(books));
+//     }
+// }
+class Store {
+    static getBooks() {
+      let books;
+      if(localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+  
+      return books;
+    }
+  
+    static addBook(book) {
+      const books = Store.getBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  
+    static removeBook(isbn) {
+      const books = Store.getBooks();
+  
+      books.forEach((book, index) => {
+        if(book.isbn === isbn) {
+          books.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
+
+//Display book event
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+//Add book event
+document.querySelector('#book-form').addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    if(title=='' || author=='' || isbn==''){
+        UI.showAlert('Please enter all values', 'danger');
+    }
+    else{
+        const book = new Book(title, author, isbn);
+        console.log(book);
+    
+        UI.addBookToList(book);
+        Store.addBook(book);
+
+        UI.clearFields();
+        UI.showAlert('Sucessfully added book!', 'success');
+    }
+});
+
+//Remove book event
+document.querySelector('#book-list').addEventListener('click', (e)=>{
+    UI.deleteBook(e.target);
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+    UI.showAlert('Book deleted', 'danger');
+})
